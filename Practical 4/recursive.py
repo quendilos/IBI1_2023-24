061bb4c2875d23e4befff3ed036ef8fa022c4844
quendilos <156736475+quendilos@users.noreply.github.com>
2024-03-06 13:21:44 +0800

    Update recursive.py

--- Practical 4/recursive.py
+++ Practical 4/recursive.py
@@ -1,11 +1,11 @@
 a = 4 # a has an initial value:4
 for i in range(0,5): 
-    print(a)
+    print(a)          
     a=2*a+3     
-                   #repeat it for 5 times
-                    #print it 
-                    # a=2*a+3, then return
-    # results:4,11,25,53,109
+                          
+                         #print it (don't forget the initial value should also be printed)
+                         # a=2*a+3, then repeat these two steps for 5 times
+# results:4,11,25,53,109
 
     
     
