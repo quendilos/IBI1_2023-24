632e832672b0aa1a869458c138f18e21cbfab706
quendilos <156736475+quendilos@users.noreply.github.com>
2024-05-21 19:06:19 +0800

    Update File_spatial_SIR.py

--- Practical 10/File_spatial_SIR.py
+++ Practical 10/File_spatial_SIR.py
@@ -1,52 +1,72 @@
-# import the necessary moudel
-import numpy as np
-import matplotlib.pyplot as plt
+# set the initialized data and figure about susceptible and infect outbreak as guide
+import numpy as np 
+import matplotlib . pyplot as plt 
+population = np. zeros ( (100 , 100) ) 
 
-# intialize the suspectible/population
-population = np.zeros((100, 100))
 
-# randomize a point for the otbreak point
-outbreak = np.random.choice(range(100), 2)
-population[outbreak[0], outbreak[1]] = 1 
-# intialize the data
+outbreak = np. random . choice (range(100) ,2) 
+population [ outbreak [0] , outbreak [ 1 ] ] = 1
+# set the diesease spread data 
 beta = 0.3  
 gamma = 0.05  
 time_steps = 100 
+neighbours=[]
+new_infected=[]
+new_recovery=[]
+# below is to output the initiative figure(i=0)
+plt.figure(figsize=(6, 4), dpi=150)
+plt.imshow(population, cmap='viridis', interpolation='nearest')
+plt.title('times:0')
+plt.show()
+#repeat the spread process for 101 times! the '1'more is for the 100th figure!
+for i in range(time_steps+1):
+#put all neighbours in a list
+    new_infected=[]
+    new_recovery=[]
+    for a in range(100):
+        for b in range(100):
+            neighbours = []
+            if population[a,b] == 1: # prevent the point out of 100
+# list all neighbour and add them  
+# if code ensure that the corner point will be included while the point out of 100 will be excluded
+               
+                    if a + 1 < 100: 
+                         neighbours.append((a + 1, b))
+                    if a - 1 >= 0: 
+                         neighbours.append((a - 1, b))
+                    if b + 1 < 100: 
+                         neighbours.append((a, b + 1))
+                    if b - 1 >= 0: 
+                         neighbours.append((a, b - 1))
+                    if a + 1 < 100 and b + 1 < 100: 
+                         neighbours.append((a + 1, b + 1))
+                    if a - 1 >= 0 and b + 1 < 100: 
+                         neighbours.append((a - 1, b + 1))
+                    if a + 1 < 100 and b - 1 >= 0: 
+                         neighbours.append((a + 1, b - 1))
+                    if a - 1 >= 0 and b - 1 >= 0:
+                        neighbours.append((a - 1, b - 1))
+                
+            for (r, q) in neighbours:
+# use an random probility code to imititate the infectqion ==0 means suspectitive audience
+                    if population[r, q] == 0 and np.random.rand() <=beta:
+                    
+                            new_infected.append((r, q))
+# then use same method toq imitate the recovery for each point
+            
+                    if np.random.rand() < gamma:
+                        new_recovery.append((a, b))
+# add them in the list to change its colour
+# set infected as 1, recovery as 2
+    for (r, q) in new_infected:
+        population[r, q] = 1
+    for (a, b) in new_recovery:
+        population[a, b] = 2
 
-#check the neighbours infected or not
-def get_neighbours(x, y, size):
-    neighbours = []
-    for i in [-1, 0, 1]:
-        for j in [-1, 0, 1]:
-            if (i != 0 or j != 0) and (0 <= x + i < size) and (0 <= y + j < size):
-                neighbours.append((x + i, y + j))
-    return neighbours
+#output the figure at 10 50 100 time points(i=0 has been outout below)
 
-#Then operate the outbreak model in the time_steps...
-for _ in range(time_steps):
-# set the intiative variables lists
-    new_infected = []
-    new_recovery = []
-    for i in range(100):
-        for j in range(100):
-            if population[i, j] == 1:
-                neighbours = get_neighbours(i, j, 100)
-# for the outbreak and determine its neigbours
-# and then determine if their neibours are infected
-                for (x, y) in neighbours:
-                    if population[x, y] == 0 and np.random.rand() < beta:
-                        new_infected.append((x, y))
-# Check RECOVERY OR NOT
-                if np.random.rand() < gamma:
-                    new_recovery.append((i, j))
-    
-#NOW that log the infected and recovert data in the set
-    for (x, y) in new_infected:
-        population[x, y] = 1
-    for (x, y) in new_recovery:
-        population[x, y] = 2
-    # output the figure
-    plt.figure(figsize=(6, 4), dpi=150)
-    plt.imshow(population, cmap='viridis', interpolation='nearest')
-    plt.title('title')
-    plt.show()
+    if i in [10, 50, 100]:
+        plt.figure(figsize=(6, 4), dpi=150)
+        plt.imshow(population, cmap='viridis', interpolation='nearest')
+        plt.title('Looking at disease spread in 2D')
+        plt.show()
